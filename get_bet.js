1e072256937e5a4b35a0b9891eeaf468bd26dda2
shaikat <sislam9@stuy.edu>
2018-10-13 08:26:37 -0400

    added new code

--- get_bet.js
+++ get_bet.js
@@ -11,15 +11,15 @@ function randomize(array){
 }
 
 function returnInput(){
+  var location = ["sex shop", "universities", "parks", "museums", "thrift shops", "bakeries", "clothing stores", "boutiques", "stadiums", "theaters", "hotels"];
+
+var action = ["Sing a song at", "Dab at", "Do a Fortnite Dance at", "Squat 20 times in", "Do fifty push-ups in",
+              "Declare your love for Jumbo in", "Run around in a circle in", "Jog at a less than leisurely pace in", "Eat a gummy bear seductively in",
+              "Perform the national anthem in", "Sing the Tufts spirit song in", "Do your best impression of Michael Scott at", "Take your next Tinder date to",
+              "Walk at an extremely slow pace in"];
     randomize(location);
     randomize(action);
-    //console.log(location);
-    //console.log(action);
+
 }
 
-// var location = {'sex shop', 'universities', 'parks', 'museums', 'thrift shops', 'bakeries', 'clothing stores', 'boutiques', 'stadiums', 'theaters', 'hotels'};
 
-var action = {'Sing a song at', 'Dab at', 'Do a Fortnite Dance at', 'Squat 20 times in', 'Do fifty push-ups in',
-              'Declare your love for Jumbo in', 'Run around in a circle in', 'Jog at a less than leisurely pace in', 'Eat a gummy bear seductively in',
-              'Perform the national anthem in', 'Sing the Tufts spirit song in', 'Do your best impression of Michael Scott at', 'Take your next Tinder date to',
-              'Walk at an extremely slow pace in'};
