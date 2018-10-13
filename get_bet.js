4abeacad28feaddb628a94f290032c9a390d180d
shaikat <sislam9@stuy.edu>
2018-10-13 08:03:48 -0400

    added shitty code

--- get_bet.js
+++ get_bet.js
@@ -1,9 +1,22 @@
-let location = {"sex shop", "universities", "parks", "museums", "thrift shops", "bakeries", "clothing stores", 
-"boutiques", "stadiums", "theaters", "hotels"};
+location = {'sex shop', 'universities', 'parks', 'museums', 'thrift shops', 'bakeries', 'clothing stores', 'boutiques', 'stadiums', 'theaters', 'hotels'};
 
-let action = {"Sing a song at", "Dab at", "Do a Fortnite Dance at", "Squat 20 times in", "Do fifty push-ups in", 
-"Declare your love for Jumbo in", "Run around in a circle in", "Jog at a less than leisurely pace in", "Eat a gummy bear seductively in",
-"Perform the national anthem in", "Sing the Tufts spirit song in", "Do your best impression of Michael Scott at", "Take your next Tinder date to",
-"Walk at an extremely slow pace in"}
+action = {'Sing a song at', 'Dab at', 'Do a Fortnite Dance at', 'Squat 20 times in', 'Do fifty push-ups in',
+              'Declare your love for Jumbo in', 'Run around in a circle in', 'Jog at a less than leisurely pace in', 'Eat a gummy bear seductively in',
+              'Perform the national anthem in', 'Sing the Tufts spirit song in', 'Do your best impression of Michael Scott at', 'Take your next Tinder date to',
+              'Walk at an extremely slow pace in'};
 
+function randomize(array){
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
 
+function returnInput(){
+    randomize(location);
+    randomize(action);
+    //console.log(location);
+    //console.log(action);
+}
