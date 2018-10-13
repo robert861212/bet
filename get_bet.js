91ad3fa194079d0bf588139365750e4006afe41b
shaikat <sislam9@stuy.edu>
2018-10-13 08:06:57 -0400

    blah

--- get_bet.js
+++ get_bet.js
@@ -1,9 +1,9 @@
-location = {'sex shop', 'universities', 'parks', 'museums', 'thrift shops', 'bakeries', 'clothing stores', 'boutiques', 'stadiums', 'theaters', 'hotels'};
+var location = ['sex shop', 'universities', 'parks', 'museums', 'thrift shops', 'bakeries', 'clothing stores', 'boutiques', 'stadiums', 'theaters', 'hotels'];
 
-action = {'Sing a song at', 'Dab at', 'Do a Fortnite Dance at', 'Squat 20 times in', 'Do fifty push-ups in',
+var action = ['Sing a song at', 'Dab at', 'Do a Fortnite Dance at', 'Squat 20 times in', 'Do fifty push-ups in',
               'Declare your love for Jumbo in', 'Run around in a circle in', 'Jog at a less than leisurely pace in', 'Eat a gummy bear seductively in',
               'Perform the national anthem in', 'Sing the Tufts spirit song in', 'Do your best impression of Michael Scott at', 'Take your next Tinder date to',
-              'Walk at an extremely slow pace in'};
+              'Walk at an extremely slow pace in'];
 
 function randomize(array){
     for (var i = array.length - 1; i > 0; i--) {
