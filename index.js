12290382fd002148142d7e81da889f7cbf4548fe
jyang17 <robert861212@gmail.com>
2018-10-13 02:11:58 -0400

    modified yelp api

--- index.js
+++ index.js
@@ -30,6 +30,114 @@ app.use(express.static(__dirname + '/public'));
 // 	}
 // });
 
+//yelp
+
+'use strict';
+
+const yelp = require('yelp-fusion');
+
+// Yelp and Google Maps API key
+const apiKey = 'SpJmEVEugu3-OUeP_w73yPHrghCRFvB31oy40Wvwz66wnXmc5I9l2iBbFLYo1Fkk6U9OqFJdxzWK2pnQH4-NVQ3_SwSI15O2TChnamybboAs0UvBqV18fIJHN0bBW3Yx';
+
+
+
+// Yelp Fusion API Client
+const client = yelp.client(apiKey);
+
+
+
+app.post('/yelp', function(request, response) {
+	response.header("Access-Control-Allow-Origin", "*");
+   	response.header("Access-Control-Allow-Headers", "X-Requested-With");
+   	response.set('Content-Type', 'text/html');
+
+   	var obj = request.body;
+   	if (obj.hasOwnProperty('lat') && obj.hasOwnProperty('lng') && obj.hasOwnProperty('category') && obj.hasOwnProperty('distance'))
+   	{
+      var lat = obj.lat;
+      var lng = obj.lng;
+      var cat = obj.category;
+      var distance = parseFloat(obj.distance);
+
+      if (cat == 'restaurant')
+      client.search({
+	    term: 'restaurants',
+	    latitude: lat,
+	    longitude: lng,
+	    open_now: true,
+	    radius : distance,
+	    limit : 50
+		}).then(response => {
+			//sending response
+		  response.send(response.jsonBody.businesses);
+		}).catch(e => {
+		  console.log(e);
+		});
+      
+
+   	}
+   	// response.sendFile("index.html", {root:__dirname});
+			
+});
+
+
+
+// client.search({
+//     term: 'restaurants',
+//     price: 1,
+//     latitude: "42.4048",
+//     longitude: "-71.1161",
+//     open_now: true,
+//     radius : 8000,
+//     limit : 40
+// }).then(response => {
+//   const firstResult = response.jsonBody.businesses;
+//   const prettyJson = JSON.stringify(firstResult, null, 4);
+//   console.log(prettyJson);
+// }).catch(e => {
+//   console.log(e);
+// });
+
+// client.search({
+//     term: 'bars',
+//     price: 1,
+//     latitude: "42.4048",
+//     longitude: "-71.1161",
+//     open_now: true,
+//     radius : 8000
+// }).then(response => {
+//     const secondResult = response.jsonBody.businesses;
+//     const prettyJson = JSON.stringify(secondResult, null, 4);
+//     console.log(prettyJson);
+// }).catch(e => {
+//     console.log(e);
+// });
+
+// client.search({
+//     term: 'shops',
+//     price: 1,
+//     latitude: "42.4048",
+//     longitude: "-71.1161",
+//     open_now: true,
+//     radius : 8000
+// }).then(response => {
+//     const thirdResult = response.jsonBody.businesses;
+//     const prettyJson = JSON.stringify(thirdResult, null, 4);
+//     console.log(prettyJson);
+// }).catch(e => {
+//     console.log(e);
+// });
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', function(request, response) {
 	response.header("Access-Control-Allow-Origin", "*");
