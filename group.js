2cb38e125b21a2ea45ec5d6f0ebb9634e7e6ce88
jyang17 <robert861212@gmail.com>
2018-10-13 01:29:42 -0400

    added cookie and geolocation

---
+++ group.js
@@ -0,0 +1,42 @@
+var myLat;
+var myLng;
+if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+		myLat = position.coords.latitude;
+		myLng = position.coords.longitude;
+		document.cookie += "lat=" + myLat + ";lng=" + myLng; 
+	    });
+	}
+else {
+		alert("your browser does not support geolocation");
+}
+
+
+
+// function deleteAllCookies() {
+//     var cookies = document.cookie.split(";");
+
+//     for (var i = 0; i < cookies.length; i++) {
+//         var cookie = cookies[i];
+//         var eqPos = cookie.indexOf("=");
+//         var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+//         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+//     }
+// }
+
+
+// function getCookie(cname) {
+//     var name = cname + "="; //Create the cookie name variable with cookie name concatenate with = sign
+//     var cArr = window.document.cookie.split(';'); //Create cookie array by split the cookie by ';'
+     
+//     //Loop through the cookies and return the cooki value if it find the cookie name
+//     for(var i=0; i<cArr.length; i++) {
+//         var c = cArr[i].trim();
+//         //If the name is the cookie string at position 0, we found the cookie and return the cookie value
+//         if (c.indexOf(name) == 0) 
+//             return c.substring(name.length, c.length);
+//     }
+     
+//     //If we get to this point, that means the cookie wasn't find in the look, we return an empty string.
+//     return "";
+// }
